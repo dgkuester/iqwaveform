9a7357b054c2398eaa8d562447e26bd2def22f14
Dan Kuester <daniel.kuester@nist.gov>
2022-10-20 14:42:12 -0600

    add dependency

--- setup.py
+++ setup.py
@@ -66,7 +66,8 @@ if __name__ == "__main__":
             "scipy",
             "numpy",
             "seaborn",
-            "labbench"
+            "labbench",
+            "sklearn"
         ]
         + PLATFORM_REQUIRES_EXTRAS,
         # entry_points=dict(
