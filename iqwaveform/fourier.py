af0be5a7202b519cb87e9fca76fffd17a0a4ea19
Dan Kuester <daniel.kuester@nist.gov>
2024-06-25 16:03:12 -0600

    simplify fourier.py

--- iqwaveform/fourier.py
+++ iqwaveform/fourier.py
@@ -121,12 +121,6 @@ def zero_pad(x: Array, pad_amt: int) -> Array:
     return xp.pad(x, pad_amt, mode='constant', constant_values=0)
 
 
-def tile_axis0(x: Array, N) -> Array:
-    """returns N copies of x along axis 0"""
-    xp = array_namespace(x)
-    return xp.tile(x.T, N).T
-
-
 def to_blocks(y: Array, size: int, truncate=False, axis=0) -> Array:
     """Returns a view on y reshaped into blocks along axis `axis`.
 
@@ -204,7 +198,7 @@ def _get_stft_axes(
     return freqs, times
 
 
-def _to_overlapping_windows(
+def _waveform_to_overlap(
     x: Array,
     window: Array,
     nperseg: int,
@@ -213,7 +207,7 @@ def _to_overlapping_windows(
     axis=0,
     out=None,
 ) -> Array:
-    """add overlapping windows at appropriate offset _to_overlapping_windows, returning a waveform.
+    """stack segments of x into overlapping windows.
 
     Compared to the underlying stft implementations in scipy and cupyx.scipy, this has been simplified
     to a reduced set of parameters for speed.
@@ -252,7 +246,8 @@ def _to_overlapping_windows(
 
     return out
 
-def _from_overlapping_windows(y: Array, noverlap: int, nperseg: int, axis=0, out=None, extra=0) -> Array:
+
+def _overlap_to_waveform(y: Array, noverlap: int, nperseg: int, axis=0, out=None, extra=0) -> Array:
     """reconstruct the time-domain waveform from the stft in y.
 
     Compared to the underlying istft implementations in scipy and cupyx.scipy, this has been simplified
@@ -277,7 +272,7 @@ def _from_overlapping_windows(y: Array, noverlap: int, nperseg: int, axis=0, out
     if out is None:
         xr = xp.empty(target_shape, dtype=y.dtype)
     elif _is_shared_arg(out):
-        xr = _empty_shared(target_shape, dtype, xp)
+        xr = _empty_shared(target_shape, dtype=y.dtype, xp=xp)
     else:
         xr = _truncated_buffer(out, target_shape)
 
@@ -527,7 +522,7 @@ def ola_filter(
     elif out in (None, 'shared'):
         out=X
 
-    return _from_overlapping_windows(
+    return _overlap_to_waveform(
         x_windows, noverlap=noverlap, nperseg=fft_size_out, axis=axis, out=out, extra=pad_out
     )
 
@@ -626,7 +621,7 @@ def stft(
     else:
         # assert fft_size % (fft_size - noverlap) == 0
 
-        x_ol = _to_overlapping_windows(
+        x_ol = _waveform_to_overlap(
             x, window=w, nperseg=nperseg, noverlap=noverlap, axis=axis, out=out
         )
 
@@ -841,11 +836,9 @@ def upsample(iq: Array, factor: int, Ts: float = None, shift_bins=0, axis=0):
 
     count = iq.shape[0] * (factor - 1) / 2
 
-    X = zero_pad(
-        X,
-        [[int(xp.floor(count)) - shift_bins, int(xp.ceil(count)) + shift_bins]]
-        + [[0, 0]] * (len(iq.shape) - 1),
-    )
+    pad = [[int(xp.floor(count)) - shift_bins, int(xp.ceil(count)) + shift_bins]]
+
+    X = zero_pad(X, pad + [[0, 0]] * (len(iq.shape) - 1))
     x = ifft(
         xp.fft.fftshift(X, axes=axis),
         axis=axis,
@@ -1003,184 +996,3 @@ def time_to_frequency(iq, Ts, window=None, axis=0):
     )
     fftfreqs = xp.fft.fftshift(xp.fft.fftfreq(X.shape[0], Ts))
     return fftfreqs, X
-
-
-def _len_guards(M):
-    """Handle small or incorrect window lengths"""
-    if int(M) != M or M < 0:
-        raise ValueError('Window length M must be a non-negative integer')
-    return M <= 1
-
-
-def _extend(M, sym):
-    """Extend window by 1 sample if needed for DFT-even symmetry"""
-    if not sym:
-        return M + 1, True
-    else:
-        return M, False
-
-
-def _truncate(w, needed):
-    """Truncate window by 1 sample if needed for DFT-even symmetry"""
-    if needed:
-        return w[:-1]
-    else:
-        return w
-
-
-def taylor(M: int, nbar: int = 4, sll: float = 30, norm=True, sym=True) -> np.ndarray:
-    """
-    Return a Taylor window.
-    The Taylor window taper function approximates the Dolph-Chebyshev window's
-    constant sidelobe level for a parameterized number of near-in sidelobes,
-    but then allows a taper beyond [2]_.
-    The SAR (synthetic aperature radar) community commonly uses Taylor
-    weighting for image formation processing because it provides strong,
-    selectable sidelobe suppression with minimum broadening of the
-    mainlobe [1]_.
-    Parameters
-    ----------
-    M : int
-        Number of points in the output window. If zero or less, an
-        empty array is returned.
-    nbar : int, optional
-        Number of nearly constant level sidelobes adjacent to the mainlobe.
-    sll : float, optional
-        Desired suppression of sidelobe level in decibels (dB) relative to the
-        DC gain of the mainlobe. This should be a positive number.
-    norm : bool, optional
-        When True (default), divides the window by the largest (middle) value
-        for odd-length windows or the value that would occur between the two
-        repeated middle values for even-length windows such that all values
-        are less than or equal to 1. When False the DC gain will remain at 1
-        (0 dB) and the sidelobes will be `sll` dB down.
-    sym : bool, optional
-        When True (default), generates a symmetric window, for use in filter
-        design.
-        When False, generates a periodic window, for use in spectral analysis.
-    Returns
-    -------
-    out : array
-        The window. When `norm` is True (default), the maximum value is
-        normalized to 1 (though the value 1 does not appear if `M` is
-        even and `sym` is True).
-    See Also
-    --------
-    chebwin, kaiser, bartlett, blackman, hamming, hanning
-    References
-    ----------
-    .. [1] W. Carrara, R. Goodman, and R. Majewski, "Spotlight Synthetic
-           Aperture Radar: Signal Processing Algorithms" Pages 512-513,
-           July 1995.
-    .. [2] Armin Doerry, "Catalog of Window Taper Functions for
-           Sidelobe Control", 2017.
-           https://www.researchgate.net/profile/Armin_Doerry/publication/316281181_Catalog_of_Window_Taper_Functions_for_Sidelobe_Control/links/58f92cb2a6fdccb121c9d54d/Catalog-of-Window-Taper-Functions-for-Sidelobe-Control.pdf
-    Examples
-    --------
-    Plot the window and its frequency response:
-    >>> from scipy import signal
-    >>> from scipy.fft import fft, fftshift
-    >>> import matplotlib.pyplot as plt
-    >>> window = signal.windows.taylor(51, nbar=20, sll=100, norm=False)
-    >>> plt.plot(window)
-    >>> plt.title('Taylor window (100 dB)')
-    >>> plt.ylabel('Amplitude')
-    >>> plt.xlabel('Sample')
-    >>> plt.figure()
-    >>> A = fft(window, 2048) / (len(window) / 2.0)
-    >>> freq = np.linspace(-0.5, 0.5, len(A))
-    >>> response = 20 * np.log10(np.abs(fftshift(A / abs(A).max())))
-    >>> plt.plot(freq, response)
-    >>> plt.axis([-0.5, 0.5, -120, 0])
-    >>> plt.title('Frequency response of the Taylor window (100 dB)')
-    >>> plt.ylabel('Normalized magnitude [dB]')
-    >>> plt.xlabel('Normalized frequency [cycles per sample]')
-    """  # noqa: E501
-
-    if _len_guards(M):
-        return np.ones(M)
-    M, needs_trunc = _extend(M, sym)
-
-    # Original text uses a negative sidelobe level parameter and then negates
-    # it in the calculation of B. To keep consistent with other methods we
-    # assume the sidelobe level parameter to be positive.
-    B = 10 ** (sll / 20)
-    A = np.arccosh(B) / np.pi
-    s2 = nbar**2 / (A**2 + (nbar - 0.5) ** 2)
-    ma = np.arange(1, nbar)
-
-    Fm = np.empty(nbar - 1)
-    signs = np.empty_like(ma)
-    signs[::2] = 1
-    signs[1::2] = -1
-    m2 = ma * ma
-    for mi, m in enumerate(ma):
-        numer = signs[mi] * np.prod(1 - m2[mi] / s2 / (A**2 + (ma - 0.5) ** 2))
-        denom = 2 * np.prod(1 - m2[mi] / m2[:mi]) * np.prod(1 - m2[mi] / m2[mi + 1 :])
-        Fm[mi] = numer / denom
-
-    def W(n):
-        return 1 + 2 * np.dot(
-            Fm, np.cos(2 * np.pi * ma[:, np.newaxis] * (n - M / 2.0 + 0.5) / M)
-        )
-
-    w = W(np.arange(M))
-
-    # normalize (Note that this is not described in the original text [1])
-    if norm:
-        scale = 1.0 / W((M - 1) / 2)
-        w *= scale
-
-    return _truncate(w, needs_trunc)
-
-
-def knab(M: int, alpha, sym=True) -> np.ndarray:
-    if _len_guards(M):
-        return np.ones(M)
-    M, needs_trunc = _extend(M, sym)
-
-    t = np.linspace(-0.5, 0.5, M)
-
-    sqrt_term = np.sqrt(1 - (2 * t) ** 2)
-    w = np.sinh((np.pi * alpha) * sqrt_term) / (np.sinh(np.pi * alpha) * sqrt_term)
-
-    w[0] = w[-1] = np.pi * alpha / np.sinh(np.pi * alpha)
-    w /= np.sqrt(np.sum(w**2))
-
-    return _truncate(w, needs_trunc)
-
-
-def modified_bessel(M, alpha, sym=True):
-    if _len_guards(M):
-        return np.ones(M)
-    M, needs_trunc = _extend(M, sym)
-
-    t = np.linspace(-0.5, 0.5, M)
-
-    sqrt_term = np.sqrt(1 - (2 * t) ** 2)
-    w = special.i1((np.pi * alpha) * sqrt_term) / (
-        special.i1(np.pi * alpha) * sqrt_term
-    )
-
-    w[0] = w[-1] = 0  # np.pi*alpha/np.sinh(np.pi*alpha)
-
-    w /= np.sqrt(np.sum(w**2))
-
-    return _truncate(w, needs_trunc)
-
-
-def cosh(M: int, alpha, sym=True) -> np.ndarray:
-    if _len_guards(M):
-        return np.ones(M)
-    M, needs_trunc = _extend(M, sym)
-
-    t = np.linspace(-0.5, 0.5, M)
-
-    sqrt_term = np.sqrt(1 - (2 * t) ** 2)
-    w = np.cosh((np.pi * alpha) * sqrt_term) / (np.cosh(np.pi * alpha) * sqrt_term)
-
-    w[0] = w[-1] = 1 / np.cosh(np.pi * alpha)
-
-    w /= np.sqrt(np.sum(w**2))
-
-    return _truncate(w, needs_trunc)
---
+++ iqwaveform/windows.py
@@ -0,0 +1,184 @@
+"""Some window functions not included by scipy.signal"""
+
+import numpy as np
+from scipy import special
+
+def _len_guards(M):
+    """Handle small or incorrect window lengths"""
+    if int(M) != M or M < 0:
+        raise ValueError('Window length M must be a non-negative integer')
+    return M <= 1
+
+
+def _extend(M, sym):
+    """Extend window by 1 sample if needed for DFT-even symmetry"""
+    if not sym:
+        return M + 1, True
+    else:
+        return M, False
+
+
+def _truncate(w, needed):
+    """Truncate window by 1 sample if needed for DFT-even symmetry"""
+    if needed:
+        return w[:-1]
+    else:
+        return w
+
+
+def taylor(M: int, nbar: int = 4, sll: float = 30, norm=True, sym=True) -> np.ndarray:
+    """
+    Return a Taylor window.
+    The Taylor window taper function approximates the Dolph-Chebyshev window's
+    constant sidelobe level for a parameterized number of near-in sidelobes,
+    but then allows a taper beyond [2]_.
+    The SAR (synthetic aperature radar) community commonly uses Taylor
+    weighting for image formation processing because it provides strong,
+    selectable sidelobe suppression with minimum broadening of the
+    mainlobe [1]_.
+    Parameters
+    ----------
+    M : int
+        Number of points in the output window. If zero or less, an
+        empty array is returned.
+    nbar : int, optional
+        Number of nearly constant level sidelobes adjacent to the mainlobe.
+    sll : float, optional
+        Desired suppression of sidelobe level in decibels (dB) relative to the
+        DC gain of the mainlobe. This should be a positive number.
+    norm : bool, optional
+        When True (default), divides the window by the largest (middle) value
+        for odd-length windows or the value that would occur between the two
+        repeated middle values for even-length windows such that all values
+        are less than or equal to 1. When False the DC gain will remain at 1
+        (0 dB) and the sidelobes will be `sll` dB down.
+    sym : bool, optional
+        When True (default), generates a symmetric window, for use in filter
+        design.
+        When False, generates a periodic window, for use in spectral analysis.
+    Returns
+    -------
+    out : array
+        The window. When `norm` is True (default), the maximum value is
+        normalized to 1 (though the value 1 does not appear if `M` is
+        even and `sym` is True).
+    See Also
+    --------
+    chebwin, kaiser, bartlett, blackman, hamming, hanning
+    References
+    ----------
+    .. [1] W. Carrara, R. Goodman, and R. Majewski, "Spotlight Synthetic
+           Aperture Radar: Signal Processing Algorithms" Pages 512-513,
+           July 1995.
+    .. [2] Armin Doerry, "Catalog of Window Taper Functions for
+           Sidelobe Control", 2017.
+           https://www.researchgate.net/profile/Armin_Doerry/publication/316281181_Catalog_of_Window_Taper_Functions_for_Sidelobe_Control/links/58f92cb2a6fdccb121c9d54d/Catalog-of-Window-Taper-Functions-for-Sidelobe-Control.pdf
+    Examples
+    --------
+    Plot the window and its frequency response:
+    >>> from scipy import signal
+    >>> from scipy.fft import fft, fftshift
+    >>> import matplotlib.pyplot as plt
+    >>> window = signal.windows.taylor(51, nbar=20, sll=100, norm=False)
+    >>> plt.plot(window)
+    >>> plt.title('Taylor window (100 dB)')
+    >>> plt.ylabel('Amplitude')
+    >>> plt.xlabel('Sample')
+    >>> plt.figure()
+    >>> A = fft(window, 2048) / (len(window) / 2.0)
+    >>> freq = np.linspace(-0.5, 0.5, len(A))
+    >>> response = 20 * np.log10(np.abs(fftshift(A / abs(A).max())))
+    >>> plt.plot(freq, response)
+    >>> plt.axis([-0.5, 0.5, -120, 0])
+    >>> plt.title('Frequency response of the Taylor window (100 dB)')
+    >>> plt.ylabel('Normalized magnitude [dB]')
+    >>> plt.xlabel('Normalized frequency [cycles per sample]')
+    """  # noqa: E501
+
+    if _len_guards(M):
+        return np.ones(M)
+    M, needs_trunc = _extend(M, sym)
+
+    # Original text uses a negative sidelobe level parameter and then negates
+    # it in the calculation of B. To keep consistent with other methods we
+    # assume the sidelobe level parameter to be positive.
+    B = 10 ** (sll / 20)
+    A = np.arccosh(B) / np.pi
+    s2 = nbar**2 / (A**2 + (nbar - 0.5) ** 2)
+    ma = np.arange(1, nbar)
+
+    Fm = np.empty(nbar - 1)
+    signs = np.empty_like(ma)
+    signs[::2] = 1
+    signs[1::2] = -1
+    m2 = ma * ma
+    for mi, m in enumerate(ma):
+        numer = signs[mi] * np.prod(1 - m2[mi] / s2 / (A**2 + (ma - 0.5) ** 2))
+        denom = 2 * np.prod(1 - m2[mi] / m2[:mi]) * np.prod(1 - m2[mi] / m2[mi + 1 :])
+        Fm[mi] = numer / denom
+
+    def W(n):
+        return 1 + 2 * np.dot(
+            Fm, np.cos(2 * np.pi * ma[:, np.newaxis] * (n - M / 2.0 + 0.5) / M)
+        )
+
+    w = W(np.arange(M))
+
+    # normalize (Note that this is not described in the original text [1])
+    if norm:
+        scale = 1.0 / W((M - 1) / 2)
+        w *= scale
+
+    return _truncate(w, needs_trunc)
+
+
+def knab(M: int, alpha, sym=True) -> np.ndarray:
+    if _len_guards(M):
+        return np.ones(M)
+    M, needs_trunc = _extend(M, sym)
+
+    t = np.linspace(-0.5, 0.5, M)
+
+    sqrt_term = np.sqrt(1 - (2 * t) ** 2)
+    w = np.sinh((np.pi * alpha) * sqrt_term) / (np.sinh(np.pi * alpha) * sqrt_term)
+
+    w[0] = w[-1] = np.pi * alpha / np.sinh(np.pi * alpha)
+    w /= np.sqrt(np.sum(w**2))
+
+    return _truncate(w, needs_trunc)
+
+
+def modified_bessel(M, alpha, sym=True):
+    if _len_guards(M):
+        return np.ones(M)
+    M, needs_trunc = _extend(M, sym)
+
+    t = np.linspace(-0.5, 0.5, M)
+
+    sqrt_term = np.sqrt(1 - (2 * t) ** 2)
+    w = special.i1((np.pi * alpha) * sqrt_term) / (
+        special.i1(np.pi * alpha) * sqrt_term
+    )
+
+    w[0] = w[-1] = 0  # np.pi*alpha/np.sinh(np.pi*alpha)
+
+    w /= np.sqrt(np.sum(w**2))
+
+    return _truncate(w, needs_trunc)
+
+
+def cosh(M: int, alpha, sym=True) -> np.ndarray:
+    if _len_guards(M):
+        return np.ones(M)
+    M, needs_trunc = _extend(M, sym)
+
+    t = np.linspace(-0.5, 0.5, M)
+
+    sqrt_term = np.sqrt(1 - (2 * t) ** 2)
+    w = np.cosh((np.pi * alpha) * sqrt_term) / (np.cosh(np.pi * alpha) * sqrt_term)
+
+    w[0] = w[-1] = 1 / np.cosh(np.pi * alpha)
+
+    w /= np.sqrt(np.sum(w**2))
+
+    return _truncate(w, needs_trunc)
