correct normalization in design_fir_lpf

src/iqwaveform/fourier.py
@@ -233,13 +233,15 @@ def fftfreq(n, d, *, xp=np, dtype='float64') -> ArrayType:
         return xp.linspace(-fnyq + fnyq / n, fnyq - fnyq / n, n, dtype=dtype)
 
 
-def _enbw_uncached(window: str | tuple[str, float], N, fftbins=True, cached=True):
+def _enbw_uncached(
+    window: str | tuple[str, float], N, fftbins=True, cached=True, xp=np
+):
     """return the equivalent noise bandwidth (ENBW) of a window, in bins"""
     if cached:
-        w = get_window(window, N, fftbins=fftbins)
+        w = get_window(window, N, fftbins=fftbins, xp=xp)
     else:
-        w = _get_window_uncached(window, N, fftbins=fftbins)
-    return len(w) * np.sum(w**2) / np.sum(w) ** 2
+        w = _get_window_uncached(window, N, fftbins=fftbins, xp=xp)
+    return len(w) * xp.sum(w**2) / xp.sum(w) ** 2
 
 
 # allow access to the uncached version for find_window_param_from_enbw
@@ -250,7 +252,7 @@ equivalent_noise_bandwidth = functools.wraps(_enbw_uncached)(
 
 @functools.lru_cache()
 def find_window_param_from_enbw(
-    window_name: str, enbw: float, *, nfft: int = 4096, atol=1e-6
+    window_name: str, enbw: float, *, nfft: int = 4096, atol=1e-6, xp=np
 ) -> float:
     """find the parameter that satistifes the specified equivalent-noise bandwidth (ENBW)
     for a given single-parameter window.
@@ -277,7 +279,7 @@ def find_window_param_from_enbw(
         raise ValueError('enbw must be greater than 1')
 
     def err(x):
-        return _enbw_uncached((window_name, x), nfft, cached=False) - enbw
+        return _enbw_uncached((window_name, x), nfft, cached=False, xp=xp) - enbw
 
     if window_name == 'kaiser':
         a = np.pi * 1e-2
@@ -689,6 +691,7 @@ def design_fir_lpf(
     desired = [1, 1, 1, 0, 0, 0]
 
     b = signal.firls(numtaps, bands=bands, desired=desired, fs=sample_rate)
+    b /= xp.sqrt(xp.sum(xp.abs(b)**2))
 
     return xp.asarray(b.astype(dtype))
 